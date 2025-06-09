a60f99da0c18c0f3f6b6b818cfe86a396126952e
MichielVerschueren99 <verschueren.m@hotmail.com>
2025-06-09 17:40:16 +0200

    dutch alerts

--- js/scripts.js
+++ js/scripts.js
@@ -212,11 +212,11 @@ $(document).ready(function () {
         e.preventDefault();
         var data = $(this).serialize();
 
-        $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
+        $('#alert-wrapper').html(alert_markup('info', '<strong>Moment!</strong> Uw gegevens worden opgeslagen.'));
 
         if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
-            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
+            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Uw code in incorrect.'));
         } else {
             $.post('https://script.google.com/macros/s/AKfycbyDP4LG31W50qPKIqs7X-pNKgbJc5n2EKiI8-so0g17OhATd6LIJXQ_oVEMEYYAht6w/exec', data)
                 .done(function (data) {
@@ -230,7 +230,7 @@ $(document).ready(function () {
                 })
                 .fail(function (data) {
                     console.log(data);
-                    $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
+                    $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Er is een probleem met de server. '));
                 });
         }
     });
